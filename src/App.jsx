b417b19491adc9d740d0e1db0a36b199e7484559
Rupom <rakib.t.rupom@gmail.com>
2025-01-02 00:14:24 +0600

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
